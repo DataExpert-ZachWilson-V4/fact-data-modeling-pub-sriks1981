e7ff878b2f7ed0ef28d059ae79641437cb16f1e4
Srikanth Sudhindra <srikanthsudhindra@gmail.com>
2024-05-20 22:06:49 -0700

    Q1,2,3

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,10 +1,17 @@
+-- Cumulative user action table by device
 CREATE OR REPLACE TABLE srik1981.user_devices_cumulated (
+    -- Unique ID for each user
     user_id BIGINT,
+    -- Type of browser
     browser_type VARCHAR,
+    -- Datelist array representing active dates
     dates_active ARRAY(DATE),
+    -- Row date
     date DATE
 )
 WITH (
+    -- Store in PARQUET
     format = 'PARQUET',
+    -- Partition by Array of dates.
     partitioning = ARRAY['date']
 )
---
+++ submission/query_3.sql
@@ -0,0 +1,27 @@
+-- Incrementally load into the table user_devices_cumulated
+INSERT INTO srik1981.user_devices_cumulated
+WITH yesterday AS ( -- Build data from previous day
+    SELECT * 
+    FROM srik1981.user_devices_cumulated
+    WHERE date = DATE'2022-12-31'
+),
+today AS ( -- Build current day's data using the tables web_events and devices
+    SELECT 
+        w.user_id, 
+        d.browser_type, 
+        CAST(date_trunc('day', event_time) AS DATE) AS event_date, 
+        DATE(event_time) AS date
+    FROM bootcamp.web_events w
+    JOIN bootcamp.devices d ON w.device_id = d.device_id
+    WHERE DATE(event_time) = DATE'2023-01-01'
+) -- Build the final query for loading
+SELECT 
+    coalesce(y.user_id, t.user_id) AS user_id,
+    coalesce(y.browser_type, t.browser_type) AS browser_type,
+    CASE 
+        WHEN y.dates_active IS NOT NULL THEN ARRAY[t.event_date] || y.dates_active
+        ELSE ARRAY[t.event_date]
+    END AS dates_active,
+    t.date AS date
+FROM today t FULL OUTER JOIN yesterday y
+ON t.user_id = y.user_id
